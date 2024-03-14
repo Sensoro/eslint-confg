2cf350d51ec8f0f30d9bfa6341017a75162ce91b
wangxingkang <wang_xingkang@qq.com>
2024-03-14 15:10:55 +0800

    feat: add rules

--- src/config/types.ts
+++ src/config/types.ts
@@ -1,11 +1,16 @@
+import type { RuleEntry } from '@antfu/eslint-define-config'
+
 export interface Rule {
-  name: string;
-  value: any;
-  description: string;
-  reason: string;
-  badExample: string;
-  goodExample: string;
-  fixable: boolean;
-  extendsBaseRule: string;
-  requiresTypeChecking: boolean;
+  /** 规则名称 */
+  name: string
+  /** 规则配置 */
+  value: RuleEntry
+  /** 规则描述 */
+  description: string
+  reason: string
+  badExample: string
+  goodExample: string
+  fixable: boolean
+  extendsBaseRule: string
+  requiresTypeChecking: boolean
 }
