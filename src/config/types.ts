8b8f82c9780b9fb297fa610f50af12520c0f654e
wangxingkang <wang_xingkang@qq.com>
2024-03-14 15:23:35 +0800

    chore: update config

--- src/config/types.ts
+++ src/config/types.ts
@@ -7,10 +7,12 @@ export interface Rule {
   value: RuleEntry
   /** 规则描述 */
   description: string
+  /** 理由 */
   reason: string
+  /** 错误示例 */
   badExample: string
+  /** 正确示例 */
   goodExample: string
+  /** 是否可修复 */
   fixable: boolean
-  extendsBaseRule: string
-  requiresTypeChecking: boolean
 }
